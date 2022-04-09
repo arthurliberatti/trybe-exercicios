a55a1ffd64670510b2c8d544ed70f864da4079ab
Arthur-Liberatti <arthur_liberatti@hotmail.com>
2022-04-08 23:36:31 -0300

    exercicios 1 e 2

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/dia1.js
@@ -0,0 +1,22 @@
+// Exercicio 1
+
+const a = 20
+const b = 10
+
+console.log('Soma:' + (a + b));
+console.log('Subtração:' + (a - b));
+console.log('Multiplicação:' + (a * b));
+console.log('Divisão:' + (a / b));
+console.log('Modulo:' + (a % b));
+
+// Exercicio 2
+
+const a = 20 
+const b = 10
+
+if (a > b) {
+    console.log(" A é maior que B ");
+}
+    else {
+        console.log("B é maior que A")
+    }
